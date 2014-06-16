b5936f534e0d0c7ffa35c78bc8c670a488ac701b
liantics <liantics.antics@gmail.com>
2014-06-16 17:28:54 -0400

    Use resources for galleries

--- app/controllers/galleries_controller.rb
+++ app/controllers/galleries_controller.rb
@@ -4,4 +4,43 @@ class GalleriesController < ApplicationController
 		#
 		@galleries = Gallery.all
 	end
+
+	def show
+		@gallery = Gallery.find(params[:id])
+	end
+
+	def new
+		@gallery = Gallery.new
+	end
+
+	def create
+		gallery = Gallery.create(gallery_params)
+		redirect_to "/galleries/#{gallery.id}"
+	end
+
+	def edit
+		@gallery = Gallery.find(params[:id])
+	end
+
+	def update
+		gallery = Gallery.find(params[:id])
+		gallery.update(gallery_params)
+		redirect_to "/galleries/#{gallery.id}"
+	end
+
+	def destroy
+		gallery = Gallery.find(params[:id])
+		gallery.destroy
+		redirect_to "/"
+	end
+
+private
+	def gallery_params
+    	params.require(:gallery).permit(
+       		:name, 
+    		:description, #always add this comma, so git won't track this unchanged line when you have to add a comma after adding a new attribute to the list
+    		) #white lists the keys we want to accept
+ 	end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
-Rails.application.routes.draw do
+
   #tells rails to go to => a location and action
   #this is a domain specific language
   #that's a subset of ruby that will work with rails
@@ -6,5 +6,25 @@ Rails.application.routes.draw do
   #and the index action
 
   #the part on the left is the location, then # then name of an action
-  get "/" => "galleries#index"
+  # get "/" => "galleries#index"
+
+  # get "/galleries/new" => "galleries#new"
+
+  # patch "/galleries/:id" => "galleries#update"
+
+  # get "/galleries/:id/edit" => "galleries#edit"
+
+  # delete "/galleries/:id" => "galleries#destroy"
+
+  # get "/galleries/:id/" => "galleries#show"
+
+  # post "/galleries" => "galleries#create"
+
+  
+# The following does the same thing as ALL the lines above, and resets the root directory to the galleries root
+Rails.application.routes.draw do
+  root to: "galleries#index"
+  #white lisitng the routes we ARE allowing
+
+  resources :galleries, only: [:show, :new, :create, :edit, :update, :destroy] 
 end
