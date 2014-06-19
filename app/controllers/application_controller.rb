581db63c70764dd5302a5f8f4af96668d2f698b6
liantics <liantics.antics@gmail.com>
2014-06-19 16:44:36 -0400

    Added before action in application controller
    prevents access to things you shouldn't have unless you're logged in
    bounces you back to the login page automatically

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   #Adds the monban authentication helpers to our application
   include Monban::ControllerHelpers 
 
+  #below is the "umbrella of protection" - everyone bounces off unless logged in
+  #there is an exception in the sessions controller
+  #that skips this "before" action
+  #so people can create accounts and sign in
+  before_action :require_login #require_login is provided by monbon auth gem
+
 end
