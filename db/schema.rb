e48435dcac74b1d41e45d792bea4a2c7df79bd50
liantics <liantics.antics@gmail.com>
2014-06-19 13:06:26 -0400

    Dropped and rebuilt database
    Hopefully this will fix the disconnect between heroku and github versions of the dbs and migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140619151942) do
+ActiveRecord::Schema.define(version: 20140619170547) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,7 +23,12 @@ ActiveRecord::Schema.define(version: 20140619151942) do
     t.datetime "updated_at"
   end
 
-
+  create_table "images", force: true do |t|
+    t.integer  "gallery_id", null: false
+    t.string   "url",        null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   add_index "images", ["gallery_id"], name: "index_images_on_gallery_id", using: :btree
 
